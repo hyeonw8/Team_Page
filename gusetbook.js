abd5e311826606f1cbaf51c6726455b3ef34eba5
jongha lim <122885302+jongha1230@users.noreply.github.com>
2024-04-16 21:39:46 +0900

    Update gusetbook.js
    삭제 버튼 수정

--- gusetbook.js
+++ gusetbook.js
@@ -1,72 +1,72 @@
-// Firebase SDK 라이브러리 가져오기
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import { getFirestore, collection, addDoc, query, orderBy, getDocs, deleteDoc, doc, updateDoc, getDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+ // Firebase SDK 라이브러리 가져오기
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+ import { getFirestore, collection, addDoc, query, orderBy, getDocs, deleteDoc, doc, updateDoc, getDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
-// Firebase 구성 정보 설정
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyDk1C8F0t_ubSIX2jB3l2ijm6vALHTYSP0",
-    authDomain: "sparta-26a3f.firebaseapp.com",
-    projectId: "sparta-26a3f",
-    storageBucket: "sparta-26a3f.appspot.com",
-    messagingSenderId: "921839954023",
-    appId: "1:921839954023:web:c10addde3cf01b18b88cc2",
-    measurementId: "G-PENKWVQ51X"
-};
+ // Firebase 구성 정보 설정
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ const firebaseConfig = {
+     apiKey: "AIzaSyDk1C8F0t_ubSIX2jB3l2ijm6vALHTYSP0",
+     authDomain: "sparta-26a3f.firebaseapp.com",
+     projectId: "sparta-26a3f",
+     storageBucket: "sparta-26a3f.appspot.com",
+     messagingSenderId: "921839954023",
+     appId: "1:921839954023:web:c10addde3cf01b18b88cc2",
+     measurementId: "G-PENKWVQ51X"
+ };
 
 
-// Firebase 인스턴스 초기화
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+ // Firebase 인스턴스 초기화
+ const app = initializeApp(firebaseConfig);
+ const db = getFirestore(app);
 
-// 방명록 폼 제출 이벤트 핸들러
-document.getElementById("guestbook-form").addEventListener("submit", async (event) => {
-    event.preventDefault(); // 폼 제출 방지
+ // 방명록 폼 제출 이벤트 핸들러
+ document.getElementById("guestbook-form").addEventListener("submit", async (event) => {
+     event.preventDefault(); // 폼 제출 방지
 
-    // 입력된 값 가져오기
-    const name = document.getElementById("name").value;
-    const message = document.getElementById("message").value;
-    const password = document.getElementById("password").value;
+     // 입력된 값 가져오기
+     const name = document.getElementById("name").value;
+     const message = document.getElementById("message").value;
+     const password = document.getElementById("password").value;
 
-    try {
-        // Firestore에 데이터 추가
-        await addDoc(collection(db, "guestbook"), {
-            name: name,
-            message: message,
-            password: password,
-            timestamp: Number(new Date()) // 등록 순으로 정렬하기 위해
-        });
-        window.alert("등록 성공");
-        // 페이지 새로고침
-        window.location.reload();
-    } catch (error) {
-        // 오류 메시지 표시
-        console.error("Error adding document: ", error);
-        window.alert("방명록을 제출하는 동안 오류가 발생했습니다.");
-    }
-});
+     try {
+         // Firestore에 데이터 추가
+         await addDoc(collection(db, "guestbook"), {
+             name: name,
+             message: message,
+             password: password,
+             timestamp: Number(new Date()) // 등록 순으로 정렬하기 위해
+         });
+         window.alert("등록 성공");
+         // 페이지 새로고침
+         window.location.reload();
+     } catch (error) {
+         // 오류 메시지 표시
+         console.error("Error adding document: ", error);
+         window.alert("방명록을 제출하는 동안 오류가 발생했습니다.");
+     }
+ });
 
-// Firebase에서 방명록 항목 가져오기
-async function fetchGuestbookEntries() {
-    const querySnapshot = await getDocs(
-        query(collection(db, "guestbook"), orderBy("timestamp", "desc"))
-    );
-    // querySnapshot.forEach((doc) => {
-    //     const entry = doc.data();
-    //     displayGuestbookEntry(doc.id, entry); // 문서 ID도 전달
-    // });
-    for (const doc of querySnapshot.docs) { // 비동기 배열 순회 
-        const entry = doc.data();
-        displayGuestbookEntry(doc.id, entry);
-    }
-}
+ // Firebase에서 방명록 항목 가져오기
+ async function fetchGuestbookEntries() {
+     const querySnapshot = await getDocs(
+         query(collection(db, "guestbook"), orderBy("timestamp", "desc"))
+     );
+     // querySnapshot.forEach((doc) => {
+     //     const entry = doc.data();
+     //     displayGuestbookEntry(doc.id, entry); // 문서 ID도 전달
+     // });
+     for (const doc of querySnapshot.docs) { // 비동기 배열 순회 
+         const entry = doc.data();
+         displayGuestbookEntry(doc.id, entry);
+     }
+ }
 
-// 방명록 항목을 화면에 표시
-function displayGuestbookEntry(docId, entry) {
-    const guestbookEntriesDiv = document.getElementById("guestbook-entries");
-    const entryDiv = document.createElement("div");
-    entryDiv.classList.add("guestbook-entry");
-    entryDiv.innerHTML = `          
+ // 방명록 항목을 화면에 표시
+ function displayGuestbookEntry(docId, entry) {
+     const guestbookEntriesDiv = document.getElementById("guestbook-entries");
+     const entryDiv = document.createElement("div");
+     entryDiv.classList.add("guestbook-entry");
+     entryDiv.innerHTML = `          
      <div class="guestbook-container">  
        <div class="nameMessage">
          <p class="userName"><strong>${entry.name}</strong><br></p>
@@ -78,54 +78,53 @@ function displayGuestbookEntry(docId, entry) {
        </div>
      </div>  
      `;
-    guestbookEntriesDiv.appendChild(entryDiv);
-}
+     guestbookEntriesDiv.appendChild(entryDiv);
+ }
 
-// 삭제 버튼 클릭 이벤트 처리
-document.addEventListener("click", async (event) => {
-    if (event.target.classList.contains("delete-btn")) {
-        const promptPassword = window.prompt("삭제하시려면 비밀번호를 입력하세요.",);
-        const docId = event.target.getAttribute("data-doc-id");
-        const userInfo = await getDoc(doc(db, "guestbook", docId));
-        const userPassword = userInfo.data().password;
+ // 삭제 버튼 클릭 이벤트 처리
+ document.addEventListener("click", async (event) => {
+     if (event.target.classList.contains("delete-btn")) {
+         const promptPassword = window.prompt("삭제하시려면 비밀번호를 입력하세요.",);
+         const docId = event.target.getAttribute("data-doc-id");
+         const userInfo = await getDoc(doc(db, "guestbook", docId));
+         const userPassword = userInfo.data().password;
 
-        if (promptPassword == userPassword) {
+         if (promptPassword == userPassword) {
 
-            console.log("Deleting document with ID:", docId); // docId 출력                    
-            try {
-                // Firestore에서 문서 삭제
-                await deleteDoc(doc(db, "guestbook", docId));
-                // 삭제한 항목 제거
-                event.target.parentElement.remove();
-                console.log("Document deleted successfully."); //삭제 성공 메시지 출력
-            } catch (error) {
-                console.error("Error deleting document: ", error);
-                window.alert("방명록 항목을 삭제하는 동안 오류가 발생했습니다.");
-            }
-        } else {
-            window.alert("잘못 입력하셨습니다.");
-        }
-    }
-});
+             console.log("Deleting document with ID:", docId); // docId 출력                    
+             try {
+                 // Firestore에서 문서 삭제
+                 await deleteDoc(doc(db, "guestbook", docId));
+                 // 삭제한 항목 제거
+                 event.target.closest(".guestbook-entry").remove();
+                 console.log("Document deleted successfully."); //삭제 성공 메시지 출력
+             } catch (error) {
+                 console.error("Error deleting document: ", error);
+                 window.alert("방명록 항목을 삭제하는 동안 오류가 발생했습니다.");
+             }
+         } else {
+             window.alert("잘못 입력하셨습니다.");
+         }
+     }
+ });
 
-// 수정 버튼 클릭 이벤트 처리
-document.addEventListener("click", async (event) => {
-    if (event.target.classList.contains("edit-btn")) {
-        const docId = event.target.getAttribute("data-doc-id");
-        const parentDiv = event.target.parentElement; // 수정 버튼의 부모 요소인 div 가져오기
-        const messageSpan = parentDiv.querySelector("p"); // 해당하는 부모요소에서 메시지 <p> 요소
+ // 수정 버튼 클릭 이벤트 처리
+ document.addEventListener("click", async (event) => {
+     if (event.target.classList.contains("edit-btn")) {
+         const docId = event.target.getAttribute("data-doc-id");
+         const parentDiv = event.target.closest(".guestbook-entry");
+         const messageParagraph = parentDiv.querySelector(".userMessage");
 
-        const name = messageSpan.textContent.split(": ")[0];
-        const promptPassword = window.prompt("비밀번호를 입력하세요.",);
-        const userInfo = await getDoc(doc(db, "guestbook", docId));
-        const userPassword = userInfo.data().password;
+         const promptPassword = window.prompt("비밀번호를 입력하세요.");
+         const userInfo = await getDoc(doc(db, "guestbook", docId));
+         const userPassword = userInfo.data().password;
 
-        if (promptPassword == userPassword) {
-            // 새로운 textarea 생성
+         if (promptPassword == userPassword) {
+             // 새로운 textarea 생성
              const textarea = document.createElement("textarea");
              textarea.placeholder = "수정할 메시지를 입력하세요";
 
-            // 저장 버튼 생성
+             // 저장 버튼 생성
              const saveButton = document.createElement("button");
              saveButton.textContent = " 저장";
              saveButton.classList.add("save-btn");
@@ -164,12 +163,10 @@ document.addEventListener("click", async (event) => {
                      window.alert("메시지를 업데이트하는 동안 오류가 발생했습니다.");
                  }
              });
-        } else {
-            window.alert("비밀번호를 잘못 입력하셨습니다.");
-        }
-
-    }
-});
-
-// 페이지 로드시 방명록 항목 가져오기
-fetchGuestbookEntries();
+         } else {
+             window.alert("비밀번호를 잘못 입력하셨습니다.");
+         }
+     }
+ });
+ // 페이지 로드시 방명록 항목 가져오기
+ fetchGuestbookEntries();
