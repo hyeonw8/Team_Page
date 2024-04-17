6a8c9edf2d98248145c58de63c26f174bcb2b60f
jongha lim <122885302+jongha1230@users.noreply.github.com>
2024-04-17 10:50:59 +0900

    Add files via upload
    apikey .gitignore로 숨기기

---
+++ firebasekey.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyDk1C8F0t_ubSIX2jB3l2ijm6vALHTYSP0",
+    authDomain: "sparta-26a3f.firebaseapp.com",
+    projectId: "sparta-26a3f",
+    storageBucket: "sparta-26a3f.appspot.com",
+    messagingSenderId: "921839954023",
+    appId: "1:921839954023:web:c10addde3cf01b18b88cc2",
+    measurementId: "G-PENKWVQ51X"
+};
+
+export default config;
